afc8052cc615f2ddea3564183653dc7dc8f89f15
mermzd27 <63211940+mermzd27@users.noreply.github.com>
2020-09-02 17:35:45 +0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,13 @@
+$gcc -o main *.c
+$main
+Address of c:-467581708
+Value of c:22
+
+Address of pointer pc:-467581708
+Content of pointer pc:22
+
+Address of pointer pc:-467581708
+Content of pointer pc:11
+
+Address of c:-467581708
+Value of c:2
